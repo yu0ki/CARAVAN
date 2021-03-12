451f68bccd687ed4355cfedfcc0637147f907f74
yu0ki <i0t0o0y0u0k0i@gmail.com>
2021-03-12 06:37:13 +0000

    [Finish]アプリケーションを完成させよう-6

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -23,7 +23,7 @@ class BlogsController < ApplicationController
 
   def update
     blog = Blog.find(params[:id])
-    blog.update(params[:id])
+    blog.update(blog_params)
     redirect_to blog_path(blog)
   end
 
